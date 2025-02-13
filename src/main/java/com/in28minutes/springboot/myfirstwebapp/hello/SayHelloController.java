33e73b4625f6c72f1f68672588f063aaaddb11d7
savvy <ikechukwuakuma316@gmail.com>
2025-02-13 13:07:53 -0500

    first commit

---
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -0,0 +1,23 @@
+package com.in28minutes.springboot.myfirstwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+
+    //"say-hello -> What are you learning today?"
+
+    @RequestMapping("say-hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello! What are you learning today?";
+    }
+
+    @RequestMapping("say-hello-html")
+    @ResponseBody
+    public String sayHelloHtml() {
+        return "Hello! What are you learning today?";
+    }
+}
